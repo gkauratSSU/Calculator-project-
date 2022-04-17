3b02fd65fd8ec97a025eff51cb9693a47c2db317
gkauratSSU <77801285+gkauratSSU@users.noreply.github.com>
2022-04-17 13:57:32 -0400

    Add files via upload

--- CalcuatorFinal/src/application/MainControl.java
+++ CalcuatorFinal/src/application/MainControl.java
@@ -10,41 +10,28 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-/****
- * 
- * @author Enoc Gonzalez 
- *
- */
-
 public class MainControl {
 	
-	/**
-	 * Variable declaration and connections made to FXML on SceneBuilder,
-	 * number declaration for basic operations on calculator
-	 * x and y variables for the closing pane of calculator 
-	 * 
-	 */
-	
-	@FXML private Pane titlePane; //Pane to make closing button work
-	@FXML private ImageView bclose, bminimize; //connection of closing and minimize button
-	@FXML private Label results; //display screen of results
-	@FXML private String operator = ""; 
-	@FXML private boolean start; 
-	
-	@FXML private Button clear; 
+
+	 @FXML private Pane titlePane;
+	@FXML private ImageView bclose, bminimize;
+	@FXML private Label results;
+	private String operator = "";
+	private boolean start;
+	@FXML private Text output;
 
 
 	private long num1 = 0;
-	private long num2 = 0;
+
+
 
 	private double x;
 	private double y;
 	
 	
-	// Handle closing button and dragging calculator on the screen
+	
 	public void initialize(Stage s) {
 		
-	
 		titlePane.setOnMousePressed(mouseEvent->{
 		x = mouseEvent.getSceneX();
 		y = mouseEvent.getSceneY();
@@ -55,96 +42,57 @@ public class MainControl {
 			s.setY(mouseEvent.getScreenY()-y);
 			
 		});
-		bclose.setOnMouseClicked(mouseEvent ->{ 
+		bclose.setOnMouseClicked(mouseEvent ->{
 			s.close();
 		});
 		
 		bminimize.setOnMouseClicked(mouseEvent ->{
 			s.setIconified(true);
-		});		
-		
-} 	
+		});
+
 	
-	/**
-	 * Handles number display on the calculator by getting the number as a String and 
-	 * When start is true the screen of results is set to default, set to false to exit the if statement
-	 * @FXML connection
-	 * @param event
-	 */
-		@FXML 
+}	@FXML
 	public void Numbers(ActionEvent event) {
 			
 		if(start) {
 			results.setText("");
 			start=false;
 		}
-		
-		String value = ((Button)event.getSource()).getText(); //Get number
-		results.setText(results.getText() + value); //set number 
+		String value = ((Button)event.getSource()).getText();
+		results.setText(results.getText() + value);
 	
 	}
 
-		/**
-		 * Handles operators input into the screen
-		 * Also handles Parsing numbers from String to Long, and calls Operator methods to 
-		 * do the operations
-		 * @param event
-		 */
-	@FXML 
-	public void processOperators(ActionEvent event)  {
+	@FXML
+	public void processOperators(ActionEvent event) {
 		
-		String value = ((Button)event.getSource()).getText(); //get operator
+		String value = ((Button)event.getSource()).getText();
 		
-		
-			
 		if(!value.equals("=")) 
 		{
-			
-				
 			if(!operator.isEmpty())
 				return;
-
+			if(value.equals("C"))
+				results.setText("");
 			
 			operator = value;
 			num1 = Long.parseLong(results.getText());
 			results.setText("");
-			
 		}
-		else
+		else 
 		{
 			if(operator.isEmpty()) 
-				return;	
-			
-			num2 = Long.parseLong(results.getText());
+				return;
+				
+			long num2 = Long.parseLong(results.getText());
 			float call = Operators(num1,num2,operator);
 			results.setText(String.valueOf(call));
-			operator ="";
+			operator =" ";
 			start = true;
 		}
 	}
-	
-	/**
-	 * Clear Button method to handle clearing the screen when pressed
-	 * @param e
-	 */
-	public void clearbutton(ActionEvent e) {
-		if(e.getSource()==clear) { 
-		results.setText("");
-		//this.num1 = 0;
-		//this.num2 = 0;
-		}
-	}
-	
-	/**
-	 * Operator method handles the math done in the calculator
-	 * @param num1 is the first number passed from ProcessOperators method
-	 * @param num2 is the second number
-	 * @param operator Operator in String form passed to the method, switch case scenario to handle
-	 * operations
-	 * @return
-	 */
+
 	public float Operators(long num1, long num2, String operator) {
-		
 		
 		switch(operator) 
 		{
@@ -155,10 +103,11 @@ public class MainControl {
 		case"x":
 			return num1 * num2;
 		case"/":
-			if(num2==0) 
+			if (num2==0)
 				return 0;
-			return num1/num2;
-		
+			return num1 / num2;
+		case"C":
+			return 0;
 		default:
 			return 0;
 		
@@ -167,7 +116,5 @@ public class MainControl {
 	
 	
 	}
-	
-	
-	
-}
+	}
+
